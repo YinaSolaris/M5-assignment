618ce13fc6ee200b93c3018093c6e8c3a41c026c
YinaSolaris <yinan.zhan91@gmail.com>
2023-10-17 16:33:33 -0700

    01

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,29 @@
 //STEP 1
+function halfNumber(number) {
+    let result = number / 2;
+    console.log(`Half of ${number} is ${result}.`);
+    return result;
+  }
 
 //STEP 2
-
+function squareNumber(number) {
+    let result = number * number;
+    console.log(`The result of squaring the number ${number} is ${result}.`);
+    return result;
+  }
+  
 //STEP 3
-
+function percentOf(number1, number2) {
+    let percentage = (number1 / number2) * 100;
+    console.log(`${number1} is ${percentage}% of ${number2}.`);
+    return percentage;
+  }
+  
 //STEP 4
+function findModulus(number1, number2) {
+    let modulus = number1 % number2;
+    console.log(`${modulus} is the modulus of ${number1} and ${number2}.`);
+    return modulus;
+  }
 
 //STEP 5
